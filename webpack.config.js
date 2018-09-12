d3c3be2749e33015eb069320dcf58a9470bc0af7
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-09-12 16:30:12 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    modules: ['app', 'node_modules'],
+    extensions: [".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        include: [ path.join(__dirname, "app")]
+      }
+    ]
+  }
+};
