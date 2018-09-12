4c6132478b295f938eadef91b294d76443418461
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-09-12 16:58:56 -0600

    index.html and index.js files updated with workable flash card models, now need to resolve babel-core issue

--- app/index.js
+++ app/index.js
@@ -9,3 +9,31 @@ const component = (msg) => {
 }
 
 document.body.appendChild(component({ greeting: 'Hello', content: 'Webpack' }));
+
+var cards = []
+var Card = function(subject, answer) {
+    this.subject = subject;
+    this.answer = answer;
+}
+
+
+Card.prototype.addToCards = function() {
+  var list =   document.getElementById("cards");
+  var li = document.createElement('li');
+  li.innerHTML = this.subject + ", " + this.answer;
+  list.appendChild(li);  
+}
+
+Card.prototype.logCard = function() {
+  console.log(this.subject + " " + this.answer + " created");
+}
+
+function addCard() {
+  var input1 = document.getElementById('subject');
+  var input2 = document.getElementById('answer');
+  var person = new Person(input1.value, input2.value);
+  card.addToCards();
+  card.logCard();
+  input1.value = null;
+  input2.value = null;
+}
